a99b9609439c25c9f7fb14f4eda6b844c1171ab5
Malicious-dev <Programming1hars@gmail.com>
2023-10-13 11:02:46 +0530

    notion-clone

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import { Heading } from './_components/Heading';
+import { Heroes } from './_components/heroes';
+import { Footer } from './_components/footer';
+
+const MarkingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div
+        className="flex flex-col items-center justify-center md:justify-start text-center
+      gap-y-8 flex-1 px-6 pb-10"
+      >
+        <Heading />
+        <Heroes />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MarkingPage;
